79b63d3d94647be3b94b3eab0b6b528e7d8c45fc
nicht <miho0_0@naver.com>
2019-05-20 18:08:03 +0900

    Add files via upload

---
+++ iptables Versions/Fox_Filter_TW/FUCK_TW_iptables.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+"""
+CopyRight By 2019, Made by Nicht
+> OpenSource GNU GPL.Ver3
+
+"""
+import os
+
+cidr_old = ''
+cidr_new = 'TW_ipv4-190520.txt'
+number = 1
+
+if cidr_old:
+    with open(cidr_old) as f:
+        for i, line in enumerate(f, start=1):
+            if not line.startswith('#') or not line.strip():
+                print('{} = {}'.format(i, line.strip()))
+                os.system('iptables -D FUCKCN -p tcp -s {} -j DROP'.format(line.strip()))
+else:
+    print('file not specified')
+
+if cidr_new:
+    with open(cidr_new) as f:
+        for i, line in enumerate(f, start=1):
+            if not line.startswith('#') or not line.strip():
+                print('{} = {}'.format(i, line.strip()))
+                os.system('iptables -N FUCKCN')
+                os.system('iptables -A FUCKCN -p tcp -s {} -j DROP'.format(number, line.strip()))
+else:
+  print('file not specified')
